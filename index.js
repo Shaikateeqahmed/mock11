8ffa95e215d96b920708733a6008c32fa456925d
Shaikateeqahmed <shaikateeqahmed892@gmail.com>
2023-05-06 13:55:44 +0530

    update port

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const { connection } = require("./config/connection.js");
 const {post} = require("./routes/postroute.js");
+const cors = require("cors");
+
 require("dotenv").config();
 
 const app = express();
 
-
+app.use(cors());
 app.use(express.json());
 app.get("/",(req,res)=>{
     res.json("Home page")
